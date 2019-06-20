19010210a56d0edfcecdb03d9ef833fb6fd92da5
Camilo Payan <cpayan@gmail.com>
2019-06-20 15:11:32 -0400

    Getting all those wiki pages!

--- wiki.py
+++ wiki.py
@@ -4,20 +4,31 @@ from bs4 import BeautifulSoup
 import sqlite3
 import requests
 
-con = sqlite3.connect("./db.sqlite")
+def clean_wikipedia(text):
+    soup = BeautifulSoup(result.text, 'html.parser')
+    article = soup.find(id="mw-content-text")
+
+    content = article.find_all('p')
+    save = ""
+
+    for p in content:
+        save = save + ' ' + p.get_text()
+
+    return save
+
+con = sqlite3.connect("db.sqlite")
 cur = con.cursor()
 
 result = requests.get("https://en.wikipedia.org/wiki/Reparations_for_slavery")
+cur.execute("INSERT INTO wikipedia_pages (is_rep, article) VALUES (?, ?)", (True, clean_wikipedia(result.text)))
 
-soup = BeautifulSoup(result.text, 'html.parser')
+for x in range(1000):
+    result = requests.get("https://en.wikipedia.org/wiki/Special:Random")
+    cur.execute(
+        "INSERT INTO wikipedia_pages (is_rep, article) VALUES (?, ?)",
+        (False, clean_wikipedia(result.text))
+    )
 
-article = soup.find(id="mw-content-text")
-
-content = article.find_all('p')
-save = ""
-
-for p in content:
-    print(p.get_text())
-    save = save + ' ' + p.get_text()
-
-cur.execute('insert into wikipedia_pages (is_rep, article) VALUES (?, ?)', (True, save))
+con.commit()
+cur.close()
+con.close()
